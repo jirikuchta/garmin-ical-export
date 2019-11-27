a6be991b1ee85ad25358ba678c7a8d9ad351bc26
Jiri Kuchta <jiri.kuchta@firma.seznam.cz>
2019-11-27 18:49:16 +0100

    imporove format_utils methods signature

--- format_utils.py
+++ format_utils.py
@@ -1,34 +1,37 @@
 #!/usr/bin/env python3
 
-from typing import Union
 
-
-def time(seconds: Union[float, int]) -> str:
+def time(seconds: float) -> str:
     hours, remainder = divmod(round(seconds), 3600)
     minutes, seconds = divmod(remainder, 60)
     return f"{hours:02}:{minutes:02}:{seconds:02}"
 
 
-def distance_metres(distance: Union[float, int]) -> str:
-    return f"{round(distance)} m"
+def distance_metres(meters: float) -> str:
+    return f"{round(meters)} m"
 
 
-def distance_kilometers(distance: Union[float, int]) -> str:
-    return f"{round(distance / 1000, 2)} km"
+def distance_kilometers(meters: float) -> str:
+    return f"{round(meters / 1000, 2)} km"
 
 
-def distance_miles(distance: Union[float, int]) -> str:
-    return f"{round((distance / 1000) * 0.62137, 2)} mi"
+def distance_miles(meters: float) -> str:
+    return f"{round((meters / 1000) * 0.62137, 2)} mi"
 
 
-def speed_kmph(speed: Union[float, int]) -> str:
-    return f"{round(speed * 3.6, 1)} km/h"
+def speed_kmph(meters_per_second: float) -> str:
+    return f"{round(meters_per_second * 3.6, 1)} km/h"
 
 
-def speed_mph(speed: Union[float, int]) -> str:
-    return f"{round((speed * 3.6) * 0.62137, 1)} mi/h"
+def speed_mph(meters_per_second: float) -> str:
+    return f"{round((meters_per_second * 3.6) * 0.62137, 1)} mi/h"
 
 
-def speed_minutes_per_100_metres(speed: Union[float, int]) -> str:
-    minutes, seconds = divmod(100 / speed, 60)
+def speed_minutes_per_100_metres(meters_per_second: float) -> str:
+    minutes, seconds = divmod(100 / meters_per_second, 60)
     return f"{int(minutes)}:{round(seconds):02} min/100m"
+
+
+def speed_minutes_per_kilometer(meters_per_second: float) -> str:
+    minutes, seconds = divmod(1000 / meters_per_second, 60)
+    return f"{int(minutes)}:{round(seconds):02} min/km"
