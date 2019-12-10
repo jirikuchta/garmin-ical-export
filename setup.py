579e20f27c654c6d3d4c9696a5dbd2035dd83670
jirikuchta <jiri.kuchta@email.cz>
2019-12-10 08:49:14 +0100

    setup.py version

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ def readme():
 
 setup(
     name="garmin-ical-export",
-    version="0.0.5",
+    version="1.0.0",
     description="Export Garmin Connect activities to iCalendar file",
     long_description=readme(),
     long_description_content_type="text/markdown",
